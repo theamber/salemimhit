97e8a17d9e4fdbcd6385cbabe5b23426a532cb36
Amber Spence <amber@amberspence.org>
2019-01-26 15:11:35 -0500

    Create 2017-08-07-episode-3-3.m

---
+++ _posts/2017-08-07-episode-3-3.m
@@ -0,0 +1,18 @@
+
+---
+layout : post  
+title : Episode 3.3
+subtitle : Valerian and the City of Hot Garbage
+---
+
+## Episode 3.3 - Valerian and the City of Hot Garbage
+
+<audio controls="controls">
+	<source src="https://app.pippa.io/public/streams/59472a13808b7ecc6dfaf27e/episodes/5989146d169e4a172309fd9b.mp3" type="audio/mpeg">
+</audio>
+
+[Download](https://app.pippa.io/public/streams/59472a13808b7ecc6dfaf27e/episodes/5989146d169e4a172309fd9b.mp3)
+
+Amber and Paul suffered through Valerian and the City of A Thousand Planets.
+
+Please don't follow in our footsteps.
